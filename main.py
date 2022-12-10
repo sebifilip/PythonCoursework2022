b26c7026a0fdc3c735811f742d46747704d01300
sebifilip <sebifilip@gmail.com>
2022-12-10 14:04:15 +0000

    Update main.py

--- main.py
+++ main.py
@@ -123,7 +123,7 @@ class Printer:
         :param data: list of users and their friends to output.
         :return: None.
         """
-        all_users: dict[str, User] = data.users
+        all_users: dict[str, User.friend_names] = data.users
         for user_name in all_users:
             print(f"{user_name} -> {all_users[user_name]}")
 
